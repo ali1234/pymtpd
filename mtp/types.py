7f877b1410cc33f1baee4fe21de5bea3e785c4d1
Alistair Buxton <a.j.buxton@gmail.com>
2017-11-11 13:28:29 +0000

    MTPString should be object, not function.

--- mtp/types.py
+++ mtp/types.py
@@ -17,35 +17,37 @@ class SL(Adapter):
         return u''.join(chr(i) for i in obj)
 
 
-def MTPString():
-    return SL(Term(PrefixedArray(Byte, Int16ul)))
+MTPString = SL(Term(PrefixedArray(Byte, Int16ul)))
 
 
 mtp_device_info = Struct(
     'standard_version' / Const(Int16ul, VERSION),
     'vendor_extension_id' / Const(Int32ul, 0xffffffff),
     'version' / Default(Int16ul, 0),
-    'extensions' / Default(MTPString(), ''),
+    'extensions' / Default(MTPString, ''),
     'functional_mode' / Default(Int16ul, 0),
     'operations_supported' / Default(PrefixedArray(Int32ul, operation_code), []),
     'events_supported' / Default(PrefixedArray(Int32ul, event_code), []),
     'device_properties_supported' / Default(PrefixedArray(Int32ul, device_property_code), []),
     'capture_formats' / Default(PrefixedArray(Int32ul, format_type), []),
     'playback_formats' / Default(PrefixedArray(Int32ul, format_type), []),
-    'manufacturer' / Default(MTPString(), 'Foo Inc.'),
-    'model' / Default(MTPString(), 'Whizzotron'),
-    'device_version' / Default(MTPString(), '1.0.0'),
-    'serial_number' / Default(MTPString(), '123987564'),
+    'manufacturer' / Default(MTPString, 'Foo Inc.'),
+    'model' / Default(MTPString, 'Whizzotron'),
+    'device_version' / Default(MTPString, '1.0.0'),
+    'serial_number' / Default(MTPString, '123987564'),
 )
+
 
 
 if __name__ == '__main__':
 
-    print(MTPString().build(''))
-    print(MTPString().build('A'))
-    print(MTPString().build('hello'))
+    print(MTPString.build(''))
+    print(MTPString.build('A'))
+    print(MTPString.build('hello'))
 
-    print(MTPString().parse(b'\x00'))
-    print(MTPString().parse(b'\x02A\x00\x00\x00'))
-    print(MTPString().parse(b'\x06h\x00e\x00l\x00l\x00o\x00\x00\x00'))
+    print(MTPString.parse(b'\x00'))
+    print(MTPString.parse(b'\x02A\x00\x00\x00'))
+    print(MTPString.parse(b'\x06h\x00e\x00l\x00l\x00o\x00\x00\x00'))
 
+    import code
+    code.interact(local=locals())
