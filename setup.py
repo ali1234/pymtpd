c03d24a4f5ba3c1d249292038a5dddf3e0c0f479
Alistair Buxton <a.j.buxton@gmail.com>
2017-11-12 15:20:45 +0000

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='mtp',
+    keywords='linux usb gadget functionfs mtp',
+    version='0.1',
+    author='Alistair Buxton',
+    author_email='a.j.buxton@gmail.com',
+    url='http://github.com/ali1234/pymtpd',
+    license='GPLv3+',
+    platforms=['linux'],
+    packages=['mtp', 'mtp.types', 'mtp.properties'],
+    scripts=['pymtpd'],
+    install_requires=[
+        'functionfs', 'construct'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Hardware :: Hardware Drivers',
+    ],
+)
