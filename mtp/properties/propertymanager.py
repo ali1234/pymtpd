52cc007ab42f508e1618f94481947e213816c5ce
Alistair Buxton <a.j.buxton@gmail.com>
2017-11-12 19:03:16 +0000

    Remove debugging statements.

--- mtp/properties/propertymanager.py
+++ mtp/properties/propertymanager.py
@@ -2,9 +2,7 @@ class PropertyManager(object):
     def __init__(self, proptype, *args):
         self.__props = {}
         self.__proptype = proptype
-        print(proptype)
         for arg in args:
-            print(arg)
             self.__props[arg[0]] = self.__proptype(*arg)
 
     def keys(self):
