b261391b66b48c202e490ae98fb4fe10f9e740f7
Alistair Buxton <a.j.buxton@gmail.com>
2017-11-14 15:08:39 +0000

    Fix Enum args.

--- mtp/object.py
+++ mtp/object.py
@@ -2,5 +2,5 @@ from construct import *
 
 import mtp.constants
 
-FormatType = Enum(Int16ul, dict(mtp.constants.format_types))
-ObjectPropertyCode = Enum(Int16ul, dict(mtp.constants.object_property_codes))
+FormatType = Enum(Int16ul, **dict(mtp.constants.format_types))
+ObjectPropertyCode = Enum(Int16ul, **dict(mtp.constants.object_property_codes))
